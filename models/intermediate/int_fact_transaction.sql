4ae35dff0c49b679a1a803ad7427f93e1ba0dcfa
Thamonwan Simcharoen <namnaowna@gmail.com>
2025-05-22 05:56:17 +0000

    add update_at column in data modeling

--- models/intermediate/int_fact_transaction.sql
+++ models/intermediate/int_fact_transaction.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 WITH fact_transaction AS
 (
     SELECT ft.transaction_id,
@@ -7,9 +13,16 @@ WITH fact_transaction AS
         ft.total_spent,
         ft.payment_method,
         ft.location,
-        ft.transaction_date
+        ft.transaction_date,
+        DATETIME(TIMESTAMP(CURRENT_TIMESTAMP()), "Asia/Bangkok") AS update_at
     FROM {{ ref('stg_retail_store_cleaned') }} ft 
         LEFT JOIN {{ ref('int_dim_products') }} dp ON ft.category = dp.category AND ft.item = dp.item AND ft.price = dp.price
 )
 SELECT *
 FROM fact_transaction
+
+{% if is_incremental() %}
+
+where update_at >= (select update_at from {{ this }} )
+
+{% endif %}
